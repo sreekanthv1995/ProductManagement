fdf18925075e8f5825e48d5e61d1e4e542b880d6
Sreekanth V <sreekanthv1995@gmail.com>
2024-06-05 23:24:32 +0530

    first commit

---
+++ src/main/java/com/product_management/Management/ManagementApplication.java
@@ -0,0 +1,13 @@
+package com.product_management.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementApplication.class, args);
+	}
+
+}
